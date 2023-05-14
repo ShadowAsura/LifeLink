a0bdd1c8feff0361828c916abb426b36a38c7c18
Daniel Cui <solunian@gmail.com>
2023-05-14 16:38:30 -0700

    add arnavs favicon

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -2,6 +2,12 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+    <title>
+        LifeLink
+    </title>
+</svelte:head>
+
 <div class="from-slate-200 via-blue-200 to-sky-300 bg-gradient-to-br w-full h-full">
     <slot/>
 </div>
