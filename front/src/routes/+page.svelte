7973d217e15ab49d083efaf30c4e69561819fcde
Daniel Cui <solunian@gmail.com>
2023-05-14 11:38:47 -0700

    created general form things...

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,4 +1,54 @@
-<h1 class="text-3xl font-bold underline bg-blue-800">
-  Hello world!
-</h1>
+<script>
+
+    let fname = "", lname = "", bio = "";
+
+    const createId = (e) => {
+        console.log("created new user!");
+        console.log(crypto.randomUUID());
+    };
+</script>
+
+<form on:submit|preventDefault={createId}>
+    <div class="flex flex-row justify-center my-10">
+        <form on:submit|preventDefault={createId} class="flex flex-col w-96 border rounded p-5 gap-4">
+          <h1 class="text-3xl font-bold">
+            Create Your ID
+          </h1>
+      
+          <label for="fname">
+            First Name:
+            <input name="fname" type="text" bind:value={fname} class="border rounded-sm w-full p-1" required/>
+          </label>
+
+          <label for="lname">
+            Last Name:
+            <input name="lname" type="text" bind:value={lname} class="border rounded-sm w-full p-1" required/>
+          </label>
+      
+      
+          <div class="row row-col">
+            <label for="bio" class="block">Description:</label>
+            <textarea id="bio" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"' required
+            class="border rounded-sm w-full resize-none p-1" bind:value={bio}></textarea>
+          </div>
+      
+          <!-- <div>
+            <label for="categories">Categories:</label>
+            <select id="category-input" bind:value={category} name="categories" class="border rounded-sm">
+              {#each categories as cat}
+                <option value={cat}>{cat}</option>
+              {/each}
+            </select>
+          </div>
+      
+          <div>
+            <label for="image">Images:</label>
+            <input type="file" bind:files accept="image/png, image/jpeg"/>
+          </div> -->
+
+      
+          <button type="submit" class="border rounded-lg py-2 hover:bg-green-600 hover:text-white ease-in-out duration-100">Create</button>
+        </form>
+      </div>
+</form>
 
